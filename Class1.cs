c66c61ec34cf8529273fdf24fc9f77cb0c5a86d8
LiorShiboli <87010671+LiorShiboli@users.noreply.github.com>
2021-07-10 16:22:36 +0300

    added library

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace practice
+{
+    public class sort
+    {
+        public void Insertion(double[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int u = i;
+                while(u-1!=0&& arr[u] <= arr[u - 1])
+                {
+                    double tmp = arr[u];
+                    arr[u] = arr[u - 1];
+                    arr[u - 1] = arr[u];
+                    u--;
+                }
+            }
+        }
+
+        public void bubble()
+    }
+}
